c67ffee2510734ec7509ee0f718b9b1625213b6a
Tim Veil <tjveil@gmail.com>
2014-09-05 15:41:44 -0400

    adding storm and kafka stuff

--- storm/src/main/java/dashboard/storm/Harness.java
+++ storm/src/main/java/dashboard/storm/Harness.java
@@ -5,17 +5,14 @@ import backtype.storm.StormSubmitter;
 import backtype.storm.generated.AlreadyAliveException;
 import backtype.storm.generated.InvalidTopologyException;
 import backtype.storm.topology.TopologyBuilder;
-import com.google.common.collect.ImmutableList;
 import dashboard.storm.config.AppConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.core.env.Environment;
-import storm.kafka.BrokerHosts;
 import storm.kafka.KafkaSpout;
 import storm.kafka.SpoutConfig;
-import storm.kafka.ZkHosts;
 
 public class Harness {
 
@@ -31,21 +28,35 @@ public class Harness {
             log.debug(environment.toString());
         }
 
-
-        TopologyBuilder topologyBuilder = new TopologyBuilder();
-
         KafkaSpout kafkaSpout = new KafkaSpout(ctx.getBean(SpoutConfig.class));
 
-        topologyBuilder.setSpout("kafka-tweets", kafkaSpout);
+        TopologyBuilder topologyBuilder = new TopologyBuilder();
+        topologyBuilder.setSpout(environment.getProperty("spout.name"), kafkaSpout);
 
-        Config stormConfig = new Config();
+        Config stormConfig = buildStormConfig(environment);
+
+/*        stormConfig.put(KafkaBolt.KAFKA_BROKER_PROPERTIES, new HashMap<String, String>() {{
+            put("metadata.broker.list", topology_properties.getProperty("metadata.broker.list"));
+            put("serializer.class", "kafka.serializer.StringEncoder");
+            put("request.required.acks", "1");
+            put("producer.type", "async");
+        }});*/
 
         try {
-            StormSubmitter.submitTopology("tweet-processor", stormConfig, topologyBuilder.createTopology());
+            StormSubmitter.submitTopology(environment.getProperty("storm.topology.name"), stormConfig, topologyBuilder.createTopology());
         } catch (AlreadyAliveException | InvalidTopologyException e) {
             log.error("error", e);
         }
 
+    }
+
+    private static Config buildStormConfig(Environment environment) {
+        Config stormConfig = new Config();
+        stormConfig.setDebug(Boolean.parseBoolean(environment.getProperty("storm.debug")));
+        stormConfig.setMaxSpoutPending(Integer.parseInt(environment.getProperty("storm.max.pending")));
+        stormConfig.setNumWorkers(Integer.parseInt(environment.getProperty("storm.num.workers")));
+        stormConfig.setMessageTimeoutSecs(Integer.parseInt(environment.getProperty("storm.message.timeout.secs")));
+        return stormConfig;
     }
 
 
