bbd5856b649ec606067ae2604a49434d12927c2e
Tim Veil <tjveil@gmail.com>
2014-10-15 20:32:59 -0400

    adding solr support

--- storm/src/main/java/dashboard/storm/bolts/SolrBolt.java
+++ storm/src/main/java/dashboard/storm/bolts/SolrBolt.java
@@ -5,12 +5,15 @@ import backtype.storm.topology.BasicOutputCollector;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.base.BaseBasicBolt;
 import backtype.storm.tuple.Tuple;
+import org.apache.commons.lang.StringUtils;
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
 import org.apache.solr.common.SolrInputDocument;
+import org.apache.solr.common.SolrInputField;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.social.twitter.api.HashTagEntity;
 import org.springframework.social.twitter.api.Tweet;
 
 import java.io.IOException;
@@ -45,7 +48,23 @@ public class SolrBolt extends BaseBasicBolt {
         doc.addField("id", tweet.getId());
         doc.addField("createdAt", tweet.getCreatedAt());
         doc.addField("text", tweet.getText());
+        doc.addField("language", tweet.getLanguageCode());
         doc.addField("screenName", tweet.getUser().getScreenName());
+        doc.addField("source", tweet.getSource());
+        doc.addField("geoEnabled", tweet.getUser().getLocation());
+
+        if (tweet.hasTags()) {
+            SolrInputField field = new SolrInputField("hashtags");
+
+            for (HashTagEntity entity : tweet.getEntities().getHashTags()) {
+
+                if (StringUtils.isNotBlank(entity.getText())) {
+                    field.addValue(entity.getText(), 0f);
+                }
+            }
+        }
+
+
 
         try {
             server.add(doc);
