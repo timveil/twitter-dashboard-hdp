7186b629c5843a013172d6186523f93ed35d3be5
Tim Veil <tjveil@gmail.com>
2014-10-29 19:00:01 -0400

    packaging war with yeoman plugin, starting jetty vs maven

--- view/src/main/java/dashboard/view/config/WebConfig.java
+++ view/src/main/java/dashboard/view/config/WebConfig.java
@@ -4,18 +4,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "dashboard.view.rest")
 public class WebConfig extends WebMvcConfigurerAdapter {
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**").addResourceLocations("/static/");
-    }
 
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
