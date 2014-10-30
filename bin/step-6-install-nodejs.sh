49b6753f25385a8cd5d9857b23c2c70e459b9653
Tim Veil <tjveil@gmail.com>
2014-10-30 08:42:25 -0400

    more cleanup

--- bin/step-6-install-nodejs.sh
+++ bin/step-6-install-nodejs.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+echo ""
+echo "$(tput setaf 2)Installing NodeJS $(tput sgr 0)"
+echo ""
+
 curl -sL https://rpm.nodesource.com/setup | bash -
 yum install -y nodejs
+
+echo ""
+echo "$(tput setaf 2)Finished installing NodeJS $(tput sgr 0)"
+echo ""
