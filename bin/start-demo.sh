624dc09fcd5d2a2d6e3cfa67c3f2c375930b682c
Tim Veil <tjveil@gmail.com>
2014-10-29 23:25:52 -0400

    more cleanup

--- bin/start-demo.sh
+++ bin/start-demo.sh
@@ -7,7 +7,7 @@ echo "$(tput setaf 2)Starting Solr $(tput sgr 0)"
 echo ""
 
 start_spinner
-nohup java -jar /opt/solr/latest/hdp/start.jar -Djetty.home=/opt/solr/latest/hdp -Dsolr.solr.home=/opt/solr/latest/hdp/solr &> solr.out&
+nohup java -jar /opt/solr/latest/hdp/start.jar -Djetty.home=/opt/solr/latest/hdp -Dsolr.solr.home=/opt/solr/latest/hdp/solr &> solr.out &
 stop_spinner $?
 
 echo ""
