937c8727f7149307d15a92ea51cbd7fbae257755
Tim Veil <tjveil@gmail.com>
2014-10-30 09:52:51 -0400

    more cleanup

---
+++ bin/step-0-install-all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./step-1-install-ant.sh
+./step-2-install-maven.sh
+./step-3-install-solr.sh
+./step-4-install-banana.sh
+./step-5-install-kafka.sh
+./step-6-install-nodejs.sh
--- bin/step-6-install-nodejs.sh
+++ bin/step-6-install-nodejs.sh
@@ -5,7 +5,8 @@ echo "$(tput setaf 2)Installing NodeJS and related tools$(tput sgr 0)"
 echo ""
 
 curl -sL https://rpm.nodesource.com/setup | bash -
-yum install -y nodejs
+yum install -y nodejs gcc-c++ make ruby ruby-devel rubygems libpng-devel
+gem update --system --no-document  && gem install compass
 
 echo ""
 echo "$(tput setaf 2)Updating NPM $(tput sgr 0)"
