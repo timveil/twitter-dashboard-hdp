2e00e6a82ba046cfd46ccc406525a7d3b9d58284
Tim Veil <tjveil@gmail.com>
2014-10-30 09:03:40 -0400

    more cleanup

--- bin/step-6-install-nodejs.sh
+++ bin/step-6-install-nodejs.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
 echo ""
-echo "$(tput setaf 2)Installing NodeJS $(tput sgr 0)"
+echo "$(tput setaf 2)Installing NodeJS and related tools$(tput sgr 0)"
 echo ""
 
 curl -sL https://rpm.nodesource.com/setup | bash -
 yum install -y nodejs
 
-
+echo ""
+echo "$(tput setaf 2)Updating NPM $(tput sgr 0)"
+echo ""
 npm install -g npm@latest
-npm install -g yo
 
 echo ""
-echo "$(tput setaf 2)Finished installing NodeJS $(tput sgr 0)"
+echo "$(tput setaf 2)Installing Yeoman $(tput sgr 0)"
 echo ""
+npm install --global yo bower grunt-cli
 
+echo ""
+echo "$(tput setaf 2)Finished installing $(tput sgr 0)"
+echo ""
