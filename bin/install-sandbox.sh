90083cc69e63d32d93410abc6cefcd9b46d3da07
Tim Veil <tjveil@gmail.com>
2014-10-30 11:45:17 -0400

    more cleanup

---
+++ bin/install-sandbox.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+wget http://hortonassets.s3.amazonaws.com/2.1/virtualbox/Hortonworks_Sandbox_2.1.ova
+
+HDPNAME="Hortonworks Sandbox 2.1 - Twitter Demo"
+
+VBoxManage import Hortonworks_Sandbox_2.1.ova --vsys 0 --vmname "$HDPNAME" --vsys 0 --memory 8128 --vsys 0 --cpus 4
+
+VBoxManage modifyvm "$HDPNAME" --natpf1 storm1,tcp,127.0.0.1,9092,,9092
+VBoxManage modifyvm "$HDPNAME" --natpf1 storm2,tcp,127.0.0.1,8005,,8005
+VBoxManage modifyvm "$HDPNAME" --natpf1 solr,tcp,127.0.0.1,8983,,8983
+VBoxManage modifyvm "$HDPNAME" --natpf1 jetty,tcp,127.0.0.1,8181,,8181
+
+VBoxManage startvm "$HDPNAME"
