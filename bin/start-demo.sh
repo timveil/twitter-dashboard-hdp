9bb3ddaafd17eac6562b8dbee01b97cd485633cf
Tim Veil <tjveil@gmail.com>
2014-10-30 20:34:38 -0400

    more cleanup

--- bin/start-demo.sh
+++ bin/start-demo.sh
@@ -7,7 +7,7 @@ echo ""
 cd ..
 
 nohup java -jar /opt/solr/latest/hdp/start.jar -Djetty.home=/opt/solr/latest/hdp -Dsolr.solr.home=/opt/solr/latest/hdp/solr &> solr.out&
-sleep 5
+sleep 10
 
 echo ""
 echo "$(tput setaf 2)##### Creating 'tweets' core $(tput sgr 0)"
