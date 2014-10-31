ff663dfbbca6217d42fb915b2ed37f34264357f1
Tim Veil <tjveil@gmail.com>
2014-10-30 20:09:35 -0400

    more cleanup

--- bin/step-6-install-nodejs.sh
+++ bin/step-6-install-nodejs.sh
@@ -11,23 +11,23 @@ echo "$(tput setaf 2)##### Installing NodeJS and related tools$(tput sgr 0)"
 echo ""
 
 curl -sL https://rpm.nodesource.com/setup | bash -
-yum -q install -y -t nodejs
-yum -q install -y -t gcc-c++ make ruby ruby-devel rubygems libpng-devel
-gem update --quiet --system
-gem install compass --quiet --no-document
+yum -q install -y -t nodejs >> step-6-install-nodejs.log 2>&1
+yum -q install -y -t gcc-c++ make ruby ruby-devel rubygems libpng-devel >> step-6-install-nodejs.log 2>&1
+gem update --quiet --system >> step-6-install-nodejs.log 2>&1
+gem install compass --quiet --no-document >> step-6-install-nodejs.log 2>&1
 
 echo ""
 echo "$(tput setaf 2)##### Updating NPM $(tput sgr 0)"
 echo ""
 
-npm install -g npm@latest > step-6-install-nodejs.log.log 2>&1
+npm install -g npm@latest >> step-6-install-nodejs.log 2>&1
 
 echo ""
 echo "$(tput setaf 2)##### Installing Yeoman and related tools $(tput sgr 0)"
 echo ""
-npm install --g yo --no-color --no-insight --ignore-scripts > step-6-install-nodejs.log.log 2>&1
-npm install --g bower > step-6-install-nodejs.log.log 2>&1
-npm install --g grunt-cli > step-6-install-nodejs.log.log 2>&1
+npm install --g yo --no-color --no-insight --ignore-scripts >> step-6-install-nodejs.log 2>&1
+npm install --g bower >> step-6-install-nodejs.log 2>&1
+npm install --g grunt-cli >> step-6-install-nodejs.log 2>&1
 
 echo ""
 echo "$(tput setaf 2)##### Finished installing $(tput sgr 0)"
