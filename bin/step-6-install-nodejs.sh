072ab807828d39531c26305cd9fced27af8ad4bd
Tim Veil <tjveil@gmail.com>
2014-10-30 20:12:39 -0400

    more cleanup

--- bin/step-6-install-nodejs.sh
+++ bin/step-6-install-nodejs.sh
@@ -10,7 +10,7 @@ echo ""
 echo "$(tput setaf 2)##### Installing NodeJS and related tools$(tput sgr 0)"
 echo ""
 
-curl -sL https://rpm.nodesource.com/setup | bash -
+curl -sL https://rpm.nodesource.com/setup | bash - >> step-6-install-nodejs.log 2>&1
 yum -q install -y -t nodejs >> step-6-install-nodejs.log 2>&1
 yum -q install -y -t gcc-c++ make ruby ruby-devel rubygems libpng-devel >> step-6-install-nodejs.log 2>&1
 gem update --quiet --system >> step-6-install-nodejs.log 2>&1
