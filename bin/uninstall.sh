f50073b3a8d05d4dae8b8365daadf67ba8202b4d
Tim Veil <tjveil@gmail.com>
2014-10-30 20:21:28 -0400

    more cleanup

--- bin/uninstall.sh
+++ bin/uninstall.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+rm -rf *.log *.out
+
 rm -rf /opt/ant
 rm -rf /opt/maven
 rm -rf /opt/solr
 rm -rf /opt/banana
 
-rpm -e kafka-0.8.1.2.1.4.0-632.el6.noarch.rpm
+rpm -e kafka-0.8.1.2.1.4.0-632.el6.noarch
 
 userdel -r solr
 hadoop fs -rm -R -f /user/solr
